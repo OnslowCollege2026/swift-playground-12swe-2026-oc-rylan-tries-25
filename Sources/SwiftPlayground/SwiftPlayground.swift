9b301ef5ebb3b6a044117d91b27be77a4606ce37
oc-rylan-tries-25 <rylan.tries@student.onslow.school.nz>
2026-02-11 22:07:23 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,46 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func printDivider() {
+    print("--------")
+}
+
+func printRoomIntro() {
+    print("This program calculates room area and volume.")
+    print("It also finds the usable space after furniture.")
+}
+
 @main
 struct SwiftPlayground {
-    /// A description
-    /// - Parameters:
-    ///
     static func main() {
-        let maximumItemVolume = 2.0
+        printRoomIntro()
 
-        print("Enter room length:")
-        guard let userInput = readLine(), let roomLength = Double(userInput) else {
-
-            print("Invalid number.")
-        }
-        print("Enter room Width:")
-        guard let userInput = readLine(), let roomWidth = Double(userInput) else {
-
-            print("Enter room Height:")
-            guard let userInput = readLine(), let roomHeight = Double(userInput) else {
-                let roomArea = roomLength * roomWidth
-                let roomVolume = roomArea * roomHeight
-
-                print("Room area: \(roomArea) m^2")
-                print("Room area: \(roomVolume) m^3")
-
-                let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-
-                var totalFurnitureVolume = 0.0
-
-                furnitureVolumes.enumerated().forEach { index, volume in
-                    print("Item \(index + 1): \(volume) m^3")
-                    if volume > maximumItemVolume {
-                        print("Oversized item detected.")
-                    }
-                    totalFurnitureVolume = totalFurnitureVolume + volume
-                }
-
-                let usableVolume = roomVolume - totalFurnitureVolume
-                print("Usable volume: \(usableVolume) m^3.")
-
-            }
-        }
+        printDivider()
+print("Summary here")
+printDivider()
     }
 }
